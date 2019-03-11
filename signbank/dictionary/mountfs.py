5ef93a31d1d74c91b7c2c17aef0fae43f670e078
gstvo <gustavo.borgesfr@gmail.com>
2019-03-11 17:31:55 -0300

    get_secret corre

--- signbank/dictionary/mountfs.py
+++ signbank/dictionary/mountfs.py
@@ -1,7 +1,14 @@
 import os
 
-sshfs = get_secret("sshfs_glossimages")
-sshfs2 = get_secret("sshfs_glossvideos")
+def get_secret(secret_name):
+  try:
+    with open('/run/secrets/{0}'.format(secret_name), 'r') as secret_file:
+        return secret_file.read()
+  except IOError:
+    return None
+
+sshfs = get_secret("sign_bank_command_sshfs_images")
+sshfs2 = get_secret("sign_bank_command_sshfs_videos")
 
 os.system(sshfs)
 os.system(sshfs2)
